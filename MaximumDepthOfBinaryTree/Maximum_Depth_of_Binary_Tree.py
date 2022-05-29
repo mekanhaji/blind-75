e6a988f620bbbdcc301e7624ab4a53836acc6fdc
Jha Kanhaiya <79010342+mekanhaji@users.noreply.github.com>
2022-05-29 13:05:33 +0530

    Create Maximum_Depth_of_Binary_Tree.py
    Push Recursive DFS Sol

---
+++ MaximumDepthOfBinaryTree/Maximum_Depth_of_Binary_Tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # Recursive DFS Sol:
+        
+        # Base Case , When Reach to Leaf node or Null Node
+        if not root :
+            return 0
+        
+        # Recursive call of both side,
+        # Find max b/w them..
+        # Add 1 for Root's Depth
+        return 1 + max(self.maxDepth(root.left) , self.maxDepth(root.right))
