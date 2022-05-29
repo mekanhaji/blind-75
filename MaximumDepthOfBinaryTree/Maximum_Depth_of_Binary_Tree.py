6f4cd6ed3bf41e9779feb2a9eac41bfcb1cdc432
Jha Kanhaiya <79010342+mekanhaji@users.noreply.github.com>
2022-05-29 13:26:56 +0530

    Update Maximum_Depth_of_Binary_Tree.py
    Pushing ITERATIVE dfs

--- MaximumDepthOfBinaryTree/Maximum_Depth_of_Binary_Tree.py
+++ MaximumDepthOfBinaryTree/Maximum_Depth_of_Binary_Tree.py
@@ -4,9 +4,10 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Recursive DFS Sol:
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # Recursive DFS Sol:
         
         # Base Case , When Reach to Leaf node or Null Node
         if not root :
@@ -16,3 +17,31 @@ class Solution:
         # Find max b/w them..
         # Add 1 for Root's Depth
         return 1 + max(self.maxDepth(root.left) , self.maxDepth(root.right))
+# ------------------------------------------------------------------------------------------------------- #
+# ITERATIVE DFS Sol:
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [ [root , 1] ]
+        #  ^        ^     ^
+        # Stack   Node  Depth of that node
+        
+        # Resulten Depth 
+        res = 0
+        
+        # Stop when Stack in Empty  | Not Node left To pop from satck 
+        while stack :
+            node , depth = stack.pop()
+        # ^ currNode its Depth poped From Stack
+            
+            # If curr node isn't null
+            if node :
+                
+                # make res max of curr res and curr depth
+                res = max(res , depth)
+                
+                # add(append) child of Curr Node To Stack
+                # with + 1 to depth
+                stack.append([node.left , depth + 1])
+                stack.append([node.right , depth + 1])
+        
+        return res
