bae1b6be13aee4134ed56e36cc20e9190a07def7
Jha Kanhaiya <79010342+mekanhaji@users.noreply.github.com>
2022-05-30 20:40:06 +0530

    Update Subtree_of_Another_Tree.py

--- SubtreeOfAnotherTree/Subtree_of_Another_Tree.py
+++ SubtreeOfAnotherTree/Subtree_of_Another_Tree.py
@@ -27,3 +27,30 @@ class Solution:
         
         return (self.sameTree(p.left , q.left) and
                 self.sameTree(p.right , q.right))
+
+#--------------------------------------------------------------------------------------------------#
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        
+		#recursive function to check identity
+        def isSame(r1, r2):
+            if r1 == None and r2 == None:
+                return True
+            if r2 == None or r1 == None:
+                return False
+            else:
+                if r1.val == r2.val: #if instance looks promising, check children
+                    return isSame(r1.left, r2.left) and isSame(r1.right, r2.right)
+            return False
+        
+		#use DFS to traverse
+        q = deque([root])
+        while q:
+            cur = q.popleft()
+            if isSame(cur, subRoot):
+                return True
+            else:
+                if cur.right: q.append(cur.right)
+                if cur.left : q.append(cur.left)
+        
+        return False
