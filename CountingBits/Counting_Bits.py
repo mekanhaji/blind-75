a92db2a0f01db1719da596bf7edb61dd20bcf19c
Jha Kanhaiya <79010342+mekanhaji@users.noreply.github.com>
2022-06-02 11:52:11 +0530

    Update Counting_Bits.py

--- CountingBits/Counting_Bits.py
+++ CountingBits/Counting_Bits.py
@@ -11,3 +11,17 @@ class Solution:
             ans.append(count)
             count = 0
         return ans
+
+    
+# Sol in time -> O(n)
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        offset = 1
+        
+        for i in range(1 , n + 1) :
+            if offset * 2 == i :
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+        
+        return dp
