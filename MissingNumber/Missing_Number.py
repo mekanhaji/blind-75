dc4f82c86bdacda3bd8541e6cb3575fdbcfc9d62
Jha Kanhaiya <79010342+mekanhaji@users.noreply.github.com>
2022-06-03 10:35:19 +0530

    Update Missing_Number.py

--- MissingNumber/Missing_Number.py
+++ MissingNumber/Missing_Number.py
@@ -5,3 +5,14 @@ class Solution:
         for i in range(0 , len(nums) + 1):
             if i not in nums:
                 return i
+
+# Sorting ... time -> O(2n.logn)
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        
+        for i in range(len(nums)):
+            if nums[i] != i:
+                return i
+        return nums[-1] + 1
+# 
